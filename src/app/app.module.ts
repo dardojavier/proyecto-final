7753d16f4a12b529f7bbf58d1a25228faf37e3b5
dardojavier <dardo_bel@hotmail.com>
2022-08-21 13:20:10 -0300

    la mitad del frond end

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,29 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './component/nav/nav.component';
+import { HeaderComponent } from './component/header/header.component';
+import { TrabajosComponent } from './component/trabajos/trabajos.component';
+import { PorcentajesComponent } from './component/porcentajes/porcentajes.component';
+import { NgCircleProgressModule } from 'ng-circle-progress';
+import { ExperienciaComponent } from './component/experiencia/experiencia.component';
+import { EducacionComponent } from './component/educacion/educacion.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    HeaderComponent,
+    TrabajosComponent,
+    PorcentajesComponent,
+    ExperienciaComponent,
+    EducacionComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgCircleProgressModule.forRoot({})
   ],
   providers: [],
   bootstrap: [AppComponent]
